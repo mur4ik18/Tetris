8256d13195c3a9a22a32feab4a5e7fda1b35cbe8
mur4ik18 <o.o.kotov1999@gmail.com>
2023-12-08 09:18:36 +0100

    Add gamefield and current_piece struct

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <termios.h>
+
+#define ROWS 20
+#define COLS 10
+
+// Ici on declare notre champe de jeux, on va l'utiliser pour sauvegarder les positions de chaque piece.
+char FIELD[ROWS][COLS] = {0};
+
+typedef struct
+{
+  char x;
+  char y;
+  char *piece;
+}current_piece;
+
+
 
 int main()
 {
