816e3a6c0ddc7d5bfdf4967306c06b7e6c85144c
mur4ik18 <o.o.kotov1999@gmail.com>
2023-12-08 10:26:13 +0100

    Add function for change canonical mode to non-canonical

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -8,6 +9,7 @@
 // Ici on declare notre champe de jeux, on va l'utiliser pour sauvegarder les positions de chaque piece.
 char FIELD[ROWS][COLS] = {0};
 
+struct termios saved_attributes;
 typedef struct
 {
   char x;
@@ -16,9 +18,60 @@ typedef struct
 }current_piece;
 
 
-
-int main()
+// # ==== Terminal Input mode ==== #
+void
+reset_input_mode (void)
 {
-    printf("Hello world!\n");
-    return 0;
+  tcsetattr (STDIN_FILENO, TCSANOW, &saved_attributes);
+}
+
+void
+set_input_mode (void)
+{
+  struct termios tattr;
+  char *name;
+
+  /* Make sure stdin is a terminal. */
+  if (!isatty (STDIN_FILENO))
+    {
+      fprintf (stderr, "Not a terminal.\n");
+      exit (EXIT_FAILURE);
+    }
+
+  /* Save the terminal attributes so we can restore them later. */
+  tcgetattr (STDIN_FILENO, &saved_attributes);
+  atexit (reset_input_mode);
+
+  /* Set the funny terminal modes. */
+  tcgetattr (STDIN_FILENO, &tattr);
+  tattr.c_lflag &= ~(ICANON|ECHO); /* Clear ICANON and ECHO. */
+  tattr.c_cc[VMIN] = 1;
+  tattr.c_cc[VTIME] = 0;
+  tcsetattr (STDIN_FILENO, TCSAFLUSH, &tattr);
+}
+
+
+
+
+
+
+// # ==== MAIN ==== #
+
+int main(void)
+{
+  char option = 1;
+  char ch;
+  set_input_mode();
+  while (option)
+    {
+      // Obtenir la touche
+      read (STDIN_FILENO, &ch, 1);
+      if (ch == 113)          /* q */
+        break;
+      else
+	printf("%d\n", ch);
+    }
+ 
+  
+  return 1;
 }
